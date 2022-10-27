2a9bffcbf97a67c0b8ab074ed3ed66bed0748b5a
Ronit gujjar <92150685+Ronit-gurjar@users.noreply.github.com>
2022-10-27 22:10:04 +0530

    Update matrixmulti.py

--- matrixmulti.py
+++ matrixmulti.py
@@ -1,8 +1,8 @@
 '''Program for matrix multiplication of two matrices that are input taken from user.Developed by
 @Ronit-gurjar on github. If you liked the code be sure to satr it on github'''
-def matrixMaker(R,C,Temp):
+def matrixMaker(R,C,Temp): #Function to make Matrix made by User given Dimensions
     matrix = []
-    if Temp==1:
+    if Temp==1:            #To produce a Initial matrix(consisting of all '0') of resultant dimension matrix from the dimension of the 1st & 2nd matrices
         for i in range(R):		 
             a=[]
             for j in range(C):
@@ -11,7 +11,7 @@ def matrixMaker(R,C,Temp):
 
     else:
     
-        print("Enter the entries rowwise:")
+        print("Enter the entries rowwise:") #To create 1st & 2nd matrix
         for i in range(R):		 
             a =[]
             for j in range(C):
@@ -22,13 +22,13 @@ def matrixMaker(R,C,Temp):
 
     return matrix
 
-print("enter rows and column for 1st matrix >>:")
+print("enter rows and column for 1st matrix >>:") #For 1st matrix
 R1 = int(input("rows:"))
 C1 = int(input("columns:"))
 A=matrixMaker(R1,C1,0)
 
 
-print("enter rows and column for 2nd matrix >>:")
+print("enter rows and column for 2nd matrix >>:") #For 2nd matrix
 R2 = int(input("rows:"))
 C2 = int(input("columns:"))
 B=matrixMaker(R2,C2,0)
@@ -36,7 +36,7 @@ B=matrixMaker(R2,C2,0)
 print("matrix 1:",A)
 print("matrix 2:",B)
 
-result = matrixMaker(C1,R2,1)
+result = matrixMaker(C1,R2,1) #For resultant matrix initialization
  
 # iterating by row of A
 for i in range(len(A)):
@@ -49,4 +49,4 @@ for i in range(len(A)):
             result[i][j] += A[i][k] * B[k][j]
  
 for r in result:
-    print(r)
+    print(r)     #result matrix
